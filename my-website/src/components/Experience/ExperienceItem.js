08622baf90e9d87b7dba229cec8ca187a96bceae
David Wang <brwangdavid1027@gmail.com>
2024-02-25 18:32:02 -0500

    fix the warning by defining PropTypes for the ExperienceItem component:

--- my-website/src/components/Experience/ExperienceItem.js
+++ my-website/src/components/Experience/ExperienceItem.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types' // Import PropTypes
 import './ExperienceItem.css'
 
 const ExperienceItem = ({ title, company, companyUrl, logoUrl, location, startDate, endDate, description }) => {
@@ -6,8 +7,8 @@ const ExperienceItem = ({ title, company, companyUrl, logoUrl, location, startDa
     <div className="experience-item">
       <img src={logoUrl} alt={`${company} logo`} className="company-logo" />
       <div className="experience-details">
-        <h2>{title} </h2>
-        <h3> <a href= {companyUrl}>{company} </a> </h3>
+        <h2>{title}</h2>
+        <h3><a href={companyUrl}>{company}</a></h3>
         <p>{startDate} - {endDate} | {location}</p>
         <ul>
           {description.map((point, index) => (
@@ -17,6 +18,18 @@ const ExperienceItem = ({ title, company, companyUrl, logoUrl, location, startDa
       </div>
     </div>
   )
+}
+
+// Define the PropTypes
+ExperienceItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  company: PropTypes.string.isRequired,
+  companyUrl: PropTypes.string.isRequired,
+  logoUrl: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  startDate: PropTypes.string.isRequired,
+  endDate: PropTypes.string.isRequired,
+  description: PropTypes.arrayOf(PropTypes.string).isRequired
 }
 
 export default ExperienceItem
