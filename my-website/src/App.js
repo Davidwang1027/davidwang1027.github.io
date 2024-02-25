a5a5edfee61e9facedfa7c7cff4381e776d626ec
David Wang <brwangdavid1027@gmail.com>
2024-02-25 18:25:55 -0500

    Fix linter issue

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import Header from './components/Header/Header'
 import Experience from './components/Experience/Experience'
-import Projects from './components/Projects'
-import CourseNotes from './components/CourseNotes'
-import Personal from './components/Personal'
-import Contact from './components/Contact'
+// import Projects from './components/Projects'
+// import CourseNotes from './components/CourseNotes'
+// import Personal from './components/Personal'
+// import Contact from './components/Contact'
 // import Footer from './components/Footer';
 import { Layout, Flex } from 'antd'
 
--- my-website/src/index.js
+++ my-website/src/index.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
 // import reportWebVitals from './reportWebVitals'; // Uncomment if using web vitals
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </React.StrictMode>
+)
 
 // If you're not using a service worker, you can remove or comment this out
 // serviceWorker.unregister();
